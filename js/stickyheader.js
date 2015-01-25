304e605df7aba2a4d14e72fd94f05677828fa581
Oberst Tanja <tanja@ober.st>
2015-01-25 12:53:15 +0100

    first commit

---
+++ js/stickyheader.js
@@ -0,0 +1,20 @@
+
+$(window).scroll(function() {
+// 100 = The point you would like to fade the nav in.
+  
+	if ($(window).scrollTop() > 0 ){
+    console.log($(window).scrollTop());
+ 		$('.bg').addClass('show');
+ 		$('nav li a').addClass('navshow');
+ 		
+ 		
+    
+  } else {
+	  console.log($(window).scrollTop());
+    
+    $('.bg').removeClass('show');
+    $('nav li a').removeClass('navshow');
+    
+ 	};   	
+});
+
