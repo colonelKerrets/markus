bc72b8c862504a6b9994a7a125bbb7a27331d3af
Oberst Tanja <tanja@ober.st>
2015-02-27 17:41:15 +0100

    fixed: contact form onmouseover <input class="keepInTouch">
    doesn't look good yet

--- contact/form-data-log.php
+++
@@ -1,3 +0,0 @@
-<?php exit(); /* For security reason. To avoid public user downloading below data! */?>
-"RecordID"	"Date"	"IP"	"Email"	"Message"
-"20150224-7f33"	"2015-02-24 20:27:34"	"::1"	"taob@gmx.de"	"sdfgdgf"
